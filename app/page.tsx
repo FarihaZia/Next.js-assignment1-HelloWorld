9bad39a595ab270ae36be139b9808dad1412ee60
Fariha zia <Farihazia2006@gmail.com>
2024-09-30 20:01:23 +0500

    no change

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 export default function myName(){
   return(
-  <main className="w-full h-full bg-pink-400">
+
   <div  className=" text-center m-64">
   <h1 className="text-7xl " >
     Hello World
   </h1>
   </div>
-  </main>
   )
 }
